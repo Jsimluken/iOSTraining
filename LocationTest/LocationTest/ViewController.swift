78c7adb6ed0585aafba7cea1c16d3a30a09f4f75
Lucas Dion <simonsluke134@gmail.com>
2019-10-01 19:02:32 +0900

    Locationgit add .

---
+++ LocationTest/LocationTest/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  LocationTest
+//
+//  Created by hiroki moriguchi on 2019/10/01.
+//  Copyright © 2019 Michael Laurents. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController {
+    
+    lazy var locationManager = CLLocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        let status = CLLocationManager.authorizationStatus()
+        if status == .authorizedWhenInUse{
+            print("Yatta")
+            locationManager.distanceFilter = 10
+            locationManager.startUpdatingLocation()
+        }else if status == .authorizedAlways{
+            print("Hyper great")
+            locationManager.distanceFilter = 10
+            locationManager.startUpdatingLocation()
+        }
+    }
+
+
+}
+
+extension ViewController : CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("location size ",locations.count)
+        let location = locations.first
+        let latitude = location?.coordinate.latitude
+        let longitude = location?.coordinate.longitude
+        print("latitude: \(latitude!)\nlongitude: \(longitude!)")  
+    }
+}
+
