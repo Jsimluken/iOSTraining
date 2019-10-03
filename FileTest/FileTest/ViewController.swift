e2f0df5347641c992c847ae2efafa9cc58653946
Michael Laurents <simonsluke134@gmail.com>
2019-10-03 21:59:31 +0900

    tiredgit add .

--- FileTest/FileTest/ViewController.swift
+++ FileTest/FileTest/ViewController.swift
@@ -12,6 +12,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let fm = FileManager.default
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        print(documentsPath)
+        print(fm.fileExists(atPath: documentsPath+"/test.txt"))
+        let path = Bundle.main.path(forResource: "test", ofType: ".txt")!
+        //print(Bundle.main.path(forResource: "test", ofType: ".txt"))
+        
+        let text = try! NSString( contentsOfFile: path, encoding: String.Encoding.utf8.rawValue )
+        print(text)
+        /*NSString(contentsOf: Bundle.main.path(forResource: "test", ofType: ".txt")!, encoding: "")*/
         // Do any additional setup after loading the view.
     }
 
