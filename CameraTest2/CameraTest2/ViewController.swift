2236a2849095f618835434a2fe1775d23367fb5e
Lucas Dion <simonsluke134@gmail.com>
2019-10-04 10:18:05 +0900

    conflict

---
+++ CameraTest2/CameraTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraTest2
+//
+//  Created by hiroki moriguchi on 2019/10/02.
+//  Copyright © 2019 Michael Laurents. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ dappsSwift/Pods/Target Support Files/Pods-dappsSwift/Pods-dappsSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_dappsSwift : NSObject
+@end
+@implementation PodsDummy_Pods_dappsSwift
+@end
