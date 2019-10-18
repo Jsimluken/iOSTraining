30d0a8c3f0ba3f77b9d1f4d192e403cbe0735d12
Lucas Dion <simonsluke134@gmail.com>
2019-10-18 18:31:58 +0900

    FileTestgit add .

--- FileTest/FileTest/ViewController.swift
+++ FileTest/FileTest/ViewController.swift
@@ -21,8 +21,20 @@ class ViewController: UIViewController {
         
         let text = try! NSString( contentsOfFile: path, encoding: String.Encoding.utf8.rawValue )
         print(text)
+        
+        let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        print(documentsURL)
+        
+        let path2 = documentsURL.appendingPathComponent("test.txt")
+        
+        print(FileManager.default.createFile(atPath: path2.path, contents: nil, attributes: nil))
+        print(documentsPath,documentsURL.absoluteString)
+        
+        
+        print(FileManager.default.contents(atPath: documentsURL.path))
         /*NSString(contentsOf: Bundle.main.path(forResource: "test", ofType: ".txt")!, encoding: "")*/
         // Do any additional setup after loading the view.
+        //FileManager.default.conte
     }
 
 
